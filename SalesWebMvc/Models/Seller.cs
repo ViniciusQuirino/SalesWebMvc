fc2342838dc79b3126860b6b02900ded2de110a7
VieraMotoDelivery <viniciusquirino13@hotmail.com>
2024-09-18 15:01:54 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,17 +8,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
@@ -29,7 +37,6 @@ namespace SalesWebMvc.Models
 
         public Seller()
         {
-
         }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
@@ -56,8 +63,5 @@ namespace SalesWebMvc.Models
         {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
-
-
-
     }
 }
